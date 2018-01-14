00a234866f9b2ec4cfad14c35d18f8a8c88367d8
ALIAKBARANSARI <31079302+ALIAKBARANSARI@users.noreply.github.com>
2018-01-14 19:02:45 +0530

    Create WritableGUI.java

---
+++ WritableGUI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mychatapplication.networking;
+
+/**
+ *
+ * @author AliAkbar
+ */
+public interface WritableGUI {
+    void write(String s);
+    void setonline(int port);
+}
